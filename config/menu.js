d77f34519c544206f045c1225476466c0e3d3d9c
bmagic <baptiste@bmagic.fr>
2020-01-23 11:24:00 +0100

    Fix eslint error in menu.

--- config/menu.js
+++ config/menu.js
@@ -2,14 +2,16 @@ const menu = [
   { type: 'config', id: 'news' },
   // { type: 'config', id: 'tickets' },
   { type: 'config', id: 'tournaments' },
-  { type: 'nolink',
+  {
+    type: 'nolink',
     title: 'Infos',
     children: [
       { type: 'config', id: 'info' }
       // { type: 'external', title: 'Location', link: 'https://scoup-esport.fr/reservation/' }
       // { type: 'page', title: 'Cosplay', id: 3569, link: '/cosplay' },
       // { type: 'page', title: 'Région Occitanie', id: 3575, link: '/region' }
-    ] },
+    ]
+  },
   //  { type: 'external', title: 'ESL Pro League', link: 'https://pro.eslgaming.com/occitanie/' },
   { type: 'config', id: 'partners' }
 ]
