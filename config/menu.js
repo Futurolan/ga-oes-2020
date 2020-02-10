5085d0cbdd34d096b2b02d727d91f1539376178b
mupfpilou <41705573+mupfpilou@users.noreply.github.com>
2020-02-10 12:53:38 +0100

    Update menu.js

--- config/menu.js
+++ config/menu.js
@@ -1,7 +1,19 @@
 /* eslint-disable indent */
 const menu = [
   { type: 'config', id: 'news' },
-  { type: 'page', title: 'Road to Occitanie', id: 199603, link: '/roadtooccitanie' },
+  {
+    type: 'page',
+    title: 'Road to Occitanie',
+    id: 199603,
+    link: '/roadtooccitanie',
+    children: [
+      { type: 'page', title: 'Road to Occitanie', id: 199603, link: '/roadtooccitanie' },
+      { type: 'page', title: '28 mars - Nîmes', id: 199706, link: '/nimes' },
+      { type: 'page', title: '18 avril - Tarbes', id: 199707, link: '/tarbes' },
+      { type: 'page', title: '18 avril - Albi', id: 199708, link: '/albi' },
+      { type: 'page', title: '9 mai - Toulouse', id: 199709, link: '/toulouse' }
+    ]
+  },
   {
     type: 'nolink',
     color: '#FF00FF',
