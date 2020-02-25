14935b61b6a1fb607df40871026af491a97bd5a5
mupfpilou <41705573+mupfpilou@users.noreply.github.com>
2020-02-25 21:49:57 +0100

    Update menu.js

--- config/menu.js
+++ config/menu.js
@@ -13,28 +13,28 @@ const menu = [
       { type: 'page', title: '9 mai - Toulouse', id: 199709, link: '/toulouse' }
     ]
   },
-  { type: 'config', id: 'tournaments' },
+  // { type: 'config', id: 'tournaments' },
   {
     type: 'nolink',
     title: 'Infos',
     children: [
       { type: 'config', id: 'info' },
       { type: 'page', title: 'Hébergement', id: 199920, link: '/hebergement' },
-      { type: 'page', title: 'Comment s\'inscrire', id: 199922, link: '/inscription' },
-      { type: 'page', title: 'Location de matériel', id: 199921, link: '/location' }
+      // { type: 'page', title: 'Comment s\'inscrire', id: 199922, link: '/inscription' },
+      // { type: 'page', title: 'Location de matériel', id: 199921, link: '/location' }
     ]
   },
   //  { type: 'external', title: 'ESL Pro League', link: 'https://pro.eslgaming.com/occitanie/' },
-  { type: 'config', id: 'partners' },
-  {
-    type: 'nolink',
-    color: '#f9dc00',
-    title: 'Billetterie',
-    children: [
-      { type: 'config', color: '#f9dc00', id: 'tickets' },
-      { type: 'config', color: '#f9dc00', id: 'staticTickets' }
-    ]
-  }
+  { type: 'config', id: 'partners' }
+  // {
+    // type: 'nolink',
+    // color: '#f9dc00',
+    // title: 'Billetterie',
+    // children: [
+      // { type: 'config', color: '#f9dc00', id: 'tickets' },
+      // { type: 'config', color: '#f9dc00', id: 'staticTickets' }
+    // ]
+  // }
 ]
 
 module.exports = menu
