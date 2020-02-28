cdb46c15ac9c4208ca6ea29d5adeb0e4ebc45180
mupfpilou <41705573+mupfpilou@users.noreply.github.com>
2020-02-28 13:18:47 +0100

    Update menu.js

--- config/menu.js
+++ config/menu.js
@@ -3,9 +3,9 @@ const menu = [
   { type: 'config', id: 'news' },
   {
     type: 'page',
-    title: 'OES Charity Tour',
+    title: 'OES Tour',
     id: 199603,
-    link: '/oes-charity-tour',
+    link: '/oes-tour',
     children: [
       { type: 'page', title: '28 mars - Nîmes', id: 199706, link: '/nimes' },
       { type: 'page', title: '18 avril - Tarbes', id: 199707, link: '/tarbes' },
@@ -24,17 +24,16 @@ const menu = [
       // { type: 'page', title: 'Location de matériel', id: 199921, link: '/location' }
     ]
   },
-  //  { type: 'external', title: 'ESL Pro League', link: 'https://pro.eslgaming.com/occitanie/' },
-  { type: 'config', id: 'partners' },
-  {
-    type: 'nolink',
-    color: '#f9dc00',
-    title: 'Billetterie',
-    children: [
-      { type: 'config', color: '#f9dc00', id: 'tickets' },
-      { type: 'config', color: '#f9dc00', id: 'staticTickets' }
-    ]
-  }
+  { type: 'config', id: 'partners' }
+  // {
+    // type: 'nolink',
+    // color: '#f9dc00',
+    // title: 'Billetterie',
+    // children: [
+      // { type: 'config', color: '#f9dc00', id: 'tickets' },
+      // { type: 'config', color: '#f9dc00', id: 'staticTickets' }
+    // ]
+  // }
 ]
 
 module.exports = menu
